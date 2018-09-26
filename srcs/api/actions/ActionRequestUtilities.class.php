8e25d293405fc5f3190ae33ded28517d264bf711
Yanky <emeniyannick@gmail.com>
2018-09-26 13:14:23 +0100

    Add Actions PATCH Request

--- srcs/api/actions/ActionRequestUtilities.class.php
+++ srcs/api/actions/ActionRequestUtilities.class.php
@@ -26,15 +26,15 @@ class		ActionRequestUtilities
 		$data->id = (isset($data->id)) ? htmlspecialchars(strip_tags(trim($data->id))) : null ;
 		$data->title = (isset($data->title)) ? htmlspecialchars(strip_tags(trim($data->title))) : null ;
 		$data->street = (isset($data->street)) ? htmlspecialchars(strip_tags(trim($data->street))) : null ;
-		$data->addressInfo = (isset($data->address_info)) ? htmlspecialchars(strip_tags(trim($data->address_info))) : null ;
-		$data->codePostal = (isset($data->postal_code)) ? htmlspecialchars(strip_tags(trim($data->postal_code))) : null ;
+		$data->address_info = (isset($data->address_info)) ? htmlspecialchars(strip_tags(trim($data->address_info))) : null ;
+		$data->postal_code = (isset($data->postal_code)) ? htmlspecialchars(strip_tags(trim($data->postal_code))) : null ;
 		$data->city = (isset($data->city)) ? htmlspecialchars(strip_tags(trim($data->city))) : null ;
 		$data->country = (isset($data->country)) ? htmlspecialchars(strip_tags(trim($data->country))) : null ;
 		$data->description = (isset($data->description)) ? htmlspecialchars(strip_tags(trim($data->description))) : null ;
 		$data->date = (isset($data->date)) ? htmlspecialchars(strip_tags(trim($data->date))) : null ;
 		$data->time = (isset($data->time)) ? htmlspecialchars(strip_tags(trim($data->time))) : null ;
 		$data->duration = (isset($data->duration)) ? htmlspecialchars(strip_tags(trim($data->duration))) : null ;
-		$data->userId = (isset($data->user_id)) ? htmlspecialchars(strip_tags(trim($data->user_id))) : null ;
+		$data->user_id = (isset($data->user_id)) ? intval(htmlspecialchars(strip_tags(trim($data->user_id)))) : null ;
 
 		return ($data);
 	}
