6838e8fa86f80e5657312e2be11c032925ff2b3b
Yanky <emeniyannick@gmail.com>
2018-10-08 10:35:27 +0100

    Extras PATCH Request

--- srcs/api/extras/ExtraRequest.class.php
+++ srcs/api/extras/ExtraRequest.class.php
@@ -165,7 +165,59 @@
 			}
 			
 			// Put update action here
-			// TO DO
+			
+			ExtraRequestUtilities::SanitizeData($data);
+			
+			$query = 'UPDATE ' . $this->table . ' SET '
+			. ((isset($data->title)) ? 'title = :title,' : '')
+			. ((isset($data->street)) ? 'street = :street,' : '')
+			. ((isset($data->address_info)) ? 'address_info = :addressInfo,' : '')
+			. ((isset($data->postal_code)) ? 'postal_code = :codePostal,' : '')
+			. ((isset($data->city)) ? 'city = :city,' : '')
+			. ((isset($data->country)) ? 'coutry = :country,' : '')
+			. ((isset($data->description)) ? 'description = :description,' : '')
+			. ((isset($data->date)) ? 'date = :date,' : '')
+			. ((isset($data->time)) ? 'time = :time,' : '')
+			. ((isset($data->duration)) ? 'duration = :duration,' : '')
+			. ((isset($data->action_id)) ? 'action_id = :actionId,' : '')
+			. 'id = id'
+			. ' WHERE id = :id';			
+			
+			$conn = $db->Connect();
+			$stmt = $conn->prepare($query);
+			
+			try
+			{
+				$stmt->bindParam(":id", $id, PDO::PARAM_INT);
+				(isset($data->title)) ? $stmt->bindParam(':title', $data->title) : false;
+				(isset($data->street)) ? $stmt->bindParam(':street', $data->street) : false;
+				(isset($data->address_info)) ? $stmt->bindParam(':addressInfo', $data->address_info) : false;
+				(isset($data->postal_code)) ? $stmt->bindParam(':codePostal', $data->postal_code) : false;
+				(isset($data->city)) ? $stmt->bindParam(':city', $data->city) : false;
+				(isset($data->country)) ? $stmt->bindParam(':country', $data->country) : false;
+				(isset($data->description)) ? $stmt->bindParam(':description', $data->description) : false;
+				(isset($data->date)) ? $stmt->bindParam(':date', $data->date) : false;
+				(isset($data->time)) ? $stmt->bindParam(':time', $data->time) : false;
+				(isset($data->duration)) ? $stmt->bindParam(':duration', $data->duration) : false;
+				(isset($data->action_id)) ? $stmt->bindParam(':actionId', $data->action_id) : false;
+			}
+			catch (Exception $e)
+			{
+				internal_error("stmt->bindParam : " . $e->getMessage(),
+								__FILE__, __LINE__);
+				return (-1);
+			}
+			
+			try
+			{
+				$stmt->execute();
+			}
+			catch (Exception $e)
+			{
+				internal_error("stmt->execute : ". $e->getMessage(), __FILE__, __LINE__);
+				http_error(400, $e->getMessage());
+				return (-1);				
+			}
 			
 			http_error(200);
 		}
