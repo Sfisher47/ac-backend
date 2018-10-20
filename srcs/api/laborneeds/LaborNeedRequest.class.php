6acac0e57ec15199e782734bcfec804a1b5f9715
Yanky <emeniyannick@gmail.com>
2018-10-20 14:23:26 +0100

    Include userid when get laborneeds

--- srcs/api/laborneeds/LaborNeedRequest.class.php
+++ srcs/api/laborneeds/LaborNeedRequest.class.php
@@ -57,8 +57,7 @@
 			{
 				http_error(403);
 				return (-1);
-			}
-			
+			}			
 			
 			if (!$db)
 			{
@@ -66,9 +65,10 @@
 				return (-1);
 			}
 			
+			
 			// Get one or all laborneeds
-			$query = (!$id) ? 'SELECT * FROM ' . $this->table
-						        : "SELECT * FROM " . $this->table . " WHERE id = $id";
+			$query = (!$id) ? 'SELECT t0.* FROM ' . $this->table . " t0 JOIN Actions t1 ON t0.action_id = t1.id WHERE user_id = $auth->userid"
+						        : "SELECT t0.* FROM " . $this->table . " t0 JOIN Actions t1 ON t0.action_id = t1.id WHERE t0.id = $id AND t1.user_id = $auth->userid";
 			
 			$conn = $db->Connect();
 			$stmt = $conn->prepare($query);
