edb06b0a407e509dc5bd0cba31473a5146b3850e
Yanky <emeniyannick@gmail.com>
2018-10-22 14:46:36 +0100

    Resolve conflict

--- srcs/api/laborneeds/LaborNeedRequest.class.php
+++ srcs/api/laborneeds/LaborNeedRequest.class.php
@@ -57,8 +57,7 @@
 			{
 				http_error(403);
 				return (-1);
-			}
-			
+			}			
 			
 			if (!$db)
 			{
@@ -66,8 +65,22 @@
 				return (-1);
 			}
 			
-			// Get one or all actions
-			// TO DO
+			
+			// Get one or all laborneeds
+			$query = (!$id) ? 'SELECT t0.* FROM ' . $this->table . " t0 JOIN Actions t1 ON t0.action_id = t1.id WHERE user_id = $auth->userid"
+						        : "SELECT t0.* FROM " . $this->table . " t0 JOIN Actions t1 ON t0.action_id = t1.id WHERE t0.id = $id AND t1.user_id = $auth->userid";
+			
+			$conn = $db->Connect();
+			$stmt = $conn->prepare($query);
+			
+			$stmt->execute();
+			$ret = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			
+			if (!$ret)
+			{
+				echo '{"response" : "nothing found"}';
+				return (0);
+			}
 
 			echo json_encode($ret);
 		}
