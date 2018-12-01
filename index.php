d3723e8c96acb765582b47d5b2fde4c5b05de0be
bw-bmbarga <bmbarga@brandwatch.com>
2018-12-01 10:56:09 +0100

    Fix CORS

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
 /*             <nleme@live.fr>                                                */
 /*                                                                            */
 /*   Created: Thu Jun 28 14:15:31 2018                        by elhmn        */
-/*   Updated: Thu Nov 29 12:14:46 2018                        by bmbarga      */
+/*   Updated: Thu Nov 29 12:15:12 2018                        by bmbarga      */
 /*                                                                            */
 /* ************************************************************************** */
+
+header("Access-Control-Allow-Origin: *");
 
 define('__API_DIR__', dirname(__FILE__) . '/srcs/api');
 
@@ -24,7 +26,5 @@ require_once('srcs/api/users/UserPostUtilities.class.php');
 require_once('srcs/api/users/UserRequest.class.php');
 require_once('srcs/api/Config.class.php');
 require_once('srcs/api/router.php');
-
-header("Access-Control-Allow-Origin: *");
 
 ?>
